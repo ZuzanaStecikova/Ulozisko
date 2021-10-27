e248ce614f35d5acfa3be8fdc386245c59f80f83
ZuzanaStecikova <93225031+ZuzanaStecikova@users.noreply.github.com>
2021-10-27 11:56:14 +0200

    kapitola y programovania s Misom

---
+++ kapitola9_podmienky.py
@@ -0,0 +1,65 @@
+#is_student = True
+#i#f is_student == True:
+  #  print("He is a programmer")
+
+#is_programmer = True
+#if is_programmer == False:
+ #   print("He is a programmer")
+#elif is_student == True:
+#    print("Is student")
+#else:
+ #   print("Is a programmer and is not a student")
+
+
+#number_list1 = [1, 2, 3]
+#umber_list2 = [1, 2, 3]
+#print(number_list1 == number_list2)
+
+#is_programmer == True
+###is_student == True
+
+#if is_programmer and is_student:
+   # print("Is a student programmer")
+#else:
+   # print("Is not a student programmer")
+#if is_programmer or is_student:
+    #print("Is either student or programmer or both")
+#else:
+#   print("Is not a student nor a programmer")
+
+#if 1:
+ #   print("Its True")
+#else:
+ #   print("Its False")
+
+#if -2:
+ #print("Its True")
+#else:
+#   print("Its false")
+
+#print (1 == True)
+#print (0 == False)
+#print (2 == True)
+#print (-2 == True)
+
+#if []:
+ #   print ("Its True")
+#else:
+ #   print("Its false")
+
+
+#print([] == False)
+#print([1] == True)
+
+# podmienku vieme nap9sa5 do jedn0ho riadku
+
+is_programmer = False
+#is_programmer_string = "Is a programmer" if is_programmer else "Is not a programmer"
+#print (is_programmer_string)
+
+#Vrchny kod sa da prepisat naseldovne pomocou standardneho ifu:
+if is_programmer:
+    is_programmer_string = "He is a programmer"
+else:
+    is_programmer_string = "He is not a programmer"
+print(is_programmer_string)
